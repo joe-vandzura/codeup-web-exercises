05330671cc4250675361f744602d2fdd90366e25
Joe Vandzura <vandzura.joe.e@gmail.com>
2022-11-29 13:52:20 -0600

    Submitting split-join exercises

--- JS/planets-string.js
+++ JS/planets-string.js
@@ -2,26 +2,32 @@
     "use strict";
 
     var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
-    var planetsArray;
 
     /**
-     * TODO:
+     * TODO: DONE
      * Convert planetsString to an array, and save it in a variable named
      * planetsArray.
      * console.log planetsArray to check your work
      */
 
-    // console.log(planetsArray);
+    var planetsArray = planetsString.split("|");
+    console.log(planetsArray);
 
     /**
-     * TODO:
+     * TODO: DONE
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
      *
      * BONUS:
-     * Create another string that would display your planets in an undordered
+     * Create another string that would display your planets in an unordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+
+    var namesString = "Joey<br>Lorena<br>Candy<br>Chicken";
+    console.log(namesString);
+
+    var unorderedPlanetsString = "<ul><li>Mercury</li><li>Venus</li><li>Venus</li><li>Earth</li><li>Mars</li><li>Jupiter</li><li>Saturn</li><li>Uranus</li><li>Neptune</li></ul>";
+    console.log(unorderedPlanetsString);
 
 })();
