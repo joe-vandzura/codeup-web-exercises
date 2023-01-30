cde7ccf71efc2d652fa6cc652ad067a9eff7fbd6
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-01-30 15:33:06 -0600

    finished promises exercises

--- js/promises.js
+++ js/promises.js
@@ -1,26 +1,41 @@
-// (async () => {
-// "use strict";
-//
-//
-//
-//      function latestCommit(username) {
-//          return fetch("https://api.github.com/users/" + username + "/events", {headers: {'Authorization': 'DOCS_API_KEY'}})
-//             .then(response => response.json()) //Converting the response to a JSON object
-//             .then(data => {
-//                 return new Date(data[0].created_at);
-//             })
-//             .catch(error => console.log(error));
-//     }
-//
-//     let username = "joe-vandzura";
-//     console.log( await latestCommit(username));
-//
-//
-// })();
+"use strict";
 
-(async () => {
-    "use strict";
+const usernameInput = document.getElementById("username-input");
+const usernameButton = document.getElementById("username-button");
+
+usernameButton.addEventListener("click", () => {
+    console.log(latestCommit(usernameInput.value, DOCS_API_KEY));
+    usernameInput.value = "";
+});
+
+     function latestCommit(username, API_KEY) {
+          return fetch(`https://api.github.com/users/${username}/events`,
+             {
+                 headers:
+                     {
+                         'Authorization': `token ${API_KEY}`
+                     }
+             })
+            .then(response => response.json())
+            .then(data => {
+                return new Date(data.filter((event) => event.type === "PushEvent")[0].created_at);
+            })
+    }
 
 
+const numberInput = document.getElementById("number-input");
+const numberButton = document.getElementById("number-button");
 
-})();
+numberButton.addEventListener("click", () => {
+    const request = wait(numberInput.value);
+    request.then(message => console.log(message));
+    numberInput.value = "";
+});
+
+    function wait(number) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(`You\'ll see this after ${number/1000} seconds`);
+            }, number);
+        });
+    }
