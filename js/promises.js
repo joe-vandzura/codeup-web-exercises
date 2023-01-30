97c152ed9e996ee4620dc7dac7f4070fc00e0fc6
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-01-30 12:28:40 -0600

    submitting progress on promises exercise

---
+++ js/promises.js
@@ -0,0 +1,26 @@
+// (async () => {
+// "use strict";
+//
+//
+//
+//      function latestCommit(username) {
+//          return fetch("https://api.github.com/users/" + username + "/events", {headers: {'Authorization': 'DOCS_API_KEY'}})
+//             .then(response => response.json()) //Converting the response to a JSON object
+//             .then(data => {
+//                 return new Date(data[0].created_at);
+//             })
+//             .catch(error => console.log(error));
+//     }
+//
+//     let username = "joe-vandzura";
+//     console.log( await latestCommit(username));
+//
+//
+// })();
+
+(async () => {
+    "use strict";
+
+
+
+})();
